4c7205c6214a31f838d3d83e540fcf759c07b847
samuelsibs <samuelsebastiansibarani@gmail.com>
2024-09-17 11:32:51 +0700

    Menyelesaikan Tugas 3

--- main/views.py
+++ main/views.py
@@ -1,10 +1,43 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+from django.shortcuts import render,redirect
+from main.forms import ProductForm
+from main.models import Product
 
 def show_main(request):
+    product_entries = Product.objects.all()
+
     context = {
         'app_names' : 'Covela',
         'name': 'Samuel Sebastian Sibarani',
-        'class': 'PBP C'
+        'class': 'PBP C',
+        'product_entries': product_entries
     }
 
     return render(request, "main.html", context)
+
+def create_product_entry(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form' : form}
+    return render(request, "create_product_entry.html", context)
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request,id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request,id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
