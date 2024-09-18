72faa641b78fd8549728039aa8c89ca885157c06
samuelsibs <samuelsebastiansibarani@gmail.com>
2024-09-18 11:24:25 +0700

    Menambahkan deploy.yml

--- Covela/settings.py
+++ Covela/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://samuel-sebastian-covela.pbp.cs.ui.ac.id/", "https://samuel-sebastian-covela.pbp.cs.ui.ac.id/"]
